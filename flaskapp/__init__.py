ced7b7660b8acdebd319e11988426c3effad5738
udhay <udhaya.kumar@247.ai>
2020-11-18 21:52:34 +0530

    Small changes

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -32,6 +32,22 @@ def create_app(test_config=None):
     def hello_user(username):
         return 'Why Hello %s!' % username
 
+    @app.route('/hello/user2/<username1>')  # dynamic route
+    def hello_user(username1):
+        return 'Why Hello %s!' % username1
+
+    @app.route('/hello/user3/<username2>')  # dynamic route
+    def hello_user(username2):
+        return 'Why Hello %s!' % username2
+
+    @app.route('/hello/user4/<username3>')  # dynamic route
+    def hello_user(username3):
+        return 'Why Hello %s!' % username3
+
+    @app.route('/hello/user5/<username4>')  # dynamic route
+    def hello_user(username4):
+        return 'Why Hello %s!' % username4
+
     return app
 
 app = create_app()
